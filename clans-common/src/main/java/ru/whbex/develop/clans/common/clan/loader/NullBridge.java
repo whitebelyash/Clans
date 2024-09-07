244dd30b8674dea73391b5bf32229cc53db2b8c0
whbex <66699673+whitebelyash@users.noreply.github.com>
2024-09-07 21:00:22 +0400

    Add NullBridge

---
+++ clans-common/src/main/java/ru/whbex/develop/clans/common/clan/loader/NullBridge.java
@@ -0,0 +1,59 @@
+package ru.whbex.develop.clans.common.clan.loader;
+
+import ru.whbex.develop.clans.common.clan.Clan;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+public class NullBridge implements Bridge {
+    @Override
+    public boolean valid() {
+        return true;
+    }
+
+    @Override
+    public Clan fetchClan(String tag) {
+        return null;
+    }
+
+    @Override
+    public Clan fetchClan(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Clan> fetchAll() {
+        return List.of();
+    }
+
+    @Override
+    public UUID fetchUUIDFromTag(String tag) {
+        return null;
+    }
+
+    @Override
+    public String fetchTagFromUUID(UUID id) {
+        return null;
+    }
+
+    @Override
+    public boolean updateClan(Clan clan) {
+        return false;
+    }
+
+    @Override
+    public boolean updateAll(Collection<Clan> clans) {
+        return false;
+    }
+
+    @Override
+    public boolean insertClan(Clan clan, boolean replace) {
+        return false;
+    }
+
+    @Override
+    public boolean insertAll(Collection<Clan> clans, boolean replace) {
+        return false;
+    }
+}
