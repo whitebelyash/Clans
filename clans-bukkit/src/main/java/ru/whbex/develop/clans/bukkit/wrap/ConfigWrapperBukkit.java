24175cb8bb97470597ba84d15a299e520c02e016
whbex <66699673+whitebelyash@users.noreply.github.com>
2024-09-12 13:35:40 +0400

    ConfigWrapper: add database config

--- clans-bukkit/src/main/java/ru/whbex/develop/clans/bukkit/wrap/ConfigWrapperBukkit.java
+++ clans-bukkit/src/main/java/ru/whbex/develop/clans/bukkit/wrap/ConfigWrapperBukkit.java
@@ -1,7 +1,11 @@
 package ru.whbex.develop.clans.bukkit.wrap;
 
 import org.bukkit.configuration.Configuration;
+import org.slf4j.event.Level;
+import ru.whbex.develop.clans.common.ClansPlugin;
 import ru.whbex.develop.clans.common.wrap.ConfigWrapper;
+
+import java.util.Objects;
 
 public class ConfigWrapperBukkit implements ConfigWrapper {
     private final Configuration config;
@@ -11,5 +15,38 @@ public class ConfigWrapperBukkit implements ConfigWrapper {
     @Override
     public boolean test() {
         return false;
+    }
+
+    @Override
+    public DatabaseType getDatabaseBackend() {
+        String val = config.getString("database.type");
+        DatabaseType ret;
+        try {
+            ret = DatabaseType.valueOf(val);
+        } catch (IllegalArgumentException e){
+            ClansPlugin.log(Level.ERROR, "Invalid database type {0}! Falling back to H2", val);
+            ret = DatabaseType.H2;
+        }
+        return ret;
+    }
+
+    @Override
+    public String getDatabaseName() {
+        return Objects.requireNonNull(config.getString("database.dbname"));
+    }
+
+    @Override
+    public String getDatabaseUser() {
+        return config.getString("database.user");
+    }
+
+    @Override
+    public String getDatabasePassword() {
+        return config.getString("database.password");
+    }
+
+    @Override
+    public String getDatabaseAddress() {
+        return config.getString("database.address");
     }
 }
--- clans-common/src/main/java/ru/whbex/develop/clans/common/wrap/ConfigWrapper.java
+++ clans-common/src/main/java/ru/whbex/develop/clans/common/wrap/ConfigWrapper.java
@@ -1,6 +1,29 @@
 package ru.whbex.develop.clans.common.wrap;
 
+import ru.whbex.develop.clans.common.db.H2SQLAdapter;
+import ru.whbex.develop.clans.common.db.SQLAdapter;
+import ru.whbex.develop.clans.common.db.SQLiteAdapter;
+
 /* Simple config wrapper, i won't write overengineered shit here, don't even ask me */
 public interface ConfigWrapper {
+
+    enum DatabaseType {
+        H2(H2SQLAdapter.class),
+        SQLITE(SQLiteAdapter.class);
+
+        private final Class<? extends SQLAdapter> clazz;
+
+        DatabaseType(Class<? extends SQLAdapter> clazz){
+            this.clazz = clazz;
+        }
+    }
     boolean test();
+
+    DatabaseType getDatabaseBackend();
+    String getDatabaseName();
+    String getDatabaseUser();
+    String getDatabasePassword();
+    String getDatabaseAddress();
+
+
 }
