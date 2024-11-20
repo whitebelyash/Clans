e1a71a43467efd002376ced7233ec68c3abeb9ab
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2024-11-20 23:55:06 +0400

    Improve ClanCreateCommand

--- clans-common/src/main/java/ru/whbex/develop/clans/common/cmd/clan/ClanCreateCommand.java
+++ clans-common/src/main/java/ru/whbex/develop/clans/common/cmd/clan/ClanCreateCommand.java
@@ -8,6 +8,7 @@ import ru.whbex.develop.clans.common.cmd.CommandActor;
 import ru.whbex.develop.clans.common.cmd.exec.CommandError;
 import ru.whbex.develop.clans.common.cmd.exec.CommandUsageError;
 import ru.whbex.develop.clans.common.player.PlayerActor;
+import ru.whbex.lib.log.LogContext;
 import ru.whbex.lib.string.StringUtils;
 
 public class ClanCreateCommand implements Command {
@@ -19,13 +20,25 @@ public class ClanCreateCommand implements Command {
         if(!actor.isPlayer())
             throw new CommandError("meta.command.player-required");
         PlayerActor pa = (PlayerActor) actor;
+        // Check for clan deleted bit
+        if(cm.getClan(pa) != null && cm.getClan(pa).isDeleted()){
+            // Notify if not notified before
+            if(!pa.hasData("cmd-create-sugcont")){
+                actor.sendMessageT("command.create.suggest-recovery");
+                pa.setData("cmd-create-sugcont", new Object());
+                ClansPlugin.Context.INSTANCE.plugin.getTaskScheduler().runLater(() -> pa.removeData("cmd-create-sugcont"), 50L);
+                return;
+            }
+        }
         String tag = args[1];
         String name = StringUtils.simpleformat(Constants.CLAN_NAME_FORMAT, tag);
         ClanManager.Error e = cm.createClan(tag, name, pa.getUniqueId());
         if(e!=null){
-            // do not fix
             switch(e){
                 case CLAN_TAG_EXISTS -> throw new CommandError("command.create-clan-exists");
+                case LEAD_HAS_CLAN -> throw new CommandError("command.create.leave-leader");
+                default:
+                    LogContext.log(Level.WARN, "Unknown ClanManager error {0}. Contact developer", e);
             }
             return;
         }
