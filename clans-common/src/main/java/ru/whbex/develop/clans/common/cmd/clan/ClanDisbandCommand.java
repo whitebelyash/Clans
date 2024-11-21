2a790af0c738e19e6dfa15f3f90173ebc36f9965
whbex <66699673+whitebelyash@users.noreply.github.com>
2024-11-21 08:36:35 +0400

    ClanDisbandCommand: extract self logic to method

--- clans-common/src/main/java/ru/whbex/develop/clans/common/cmd/clan/ClanDisbandCommand.java
+++ clans-common/src/main/java/ru/whbex/develop/clans/common/cmd/clan/ClanDisbandCommand.java
@@ -14,27 +14,40 @@ import ru.whbex.lib.log.LogContext;
 public class ClanDisbandCommand implements Command {
     @Override
     public void execute(CommandActor actor, Command command, String label, String... args) {
-        // WIP
-        boolean self = false;
-        if(args.length < 2)
-            self = true;
-        PlayerActor pa = (PlayerActor) actor;
-        if(self){
-            if(ClansPlugin.Context.INSTANCE.plugin.getClanManager().getClan(pa) == null)
-                throw new CommandError("meta.command.leadership-needed");
+        if(args.length < 2){
+            // self execute
+            if(!(actor instanceof PlayerActor))
+                throw new CommandError("meta.command.player-required");
+            PlayerActor pa = (PlayerActor) actor;
+            execute_self(pa);
+        } else {
+            // other
+            // TODO: Add permission check
+            ClanManager.Error e = ClansPlugin.Context.INSTANCE.plugin.getClanManager().disbandClan(args[1]);
+            if(e!=null)
+                switch (e){
+                    case CLAN_NOT_FOUND -> throw new CommandError("meta.command.unknown-clan");
+                    case CLAN_ALR_DISBAND -> throw new CommandError("command.disband.fail-deleted");
+                    default -> LogContext.log(Level.WARN, "Unknown ClanManager error {0}. Contact developer", e);
+                }
+            actor.sendMessageT("command.disband.success");
         }
-        Clan clan = self ? ClansPlugin.Context.INSTANCE.plugin.getClanManager().getClan(pa) :
-                            ClansPlugin.Context.INSTANCE.plugin.getClanManager().getClan(args[0]);
-        if(clan == null)
-            throw new CommandError("meta.command.unknown-clan");
-        ClanManager.Error e = ClansPlugin.Context.INSTANCE.plugin.getClanManager().disbandClan(clan);
-        if(e!=null)
-            switch (e){
-                case CLAN_NOT_FOUND -> throw new CommandError("meta.command.unknown-clan");
-                case CLAN_ALR_DISBAND -> throw new CommandError(self ? "command.disband.fail-deleted" : "command.disband.fail-deleted-self");
-                default -> LogContext.log(Level.WARN, "Unknown ClanManager error {0}. Contact developer", e);
-            }
-        actor.sendMessageT(self ? "command.disband.success-self" : "command.disband.success");
+
+    }
+
+
+    // snake_case in java :skull:
+    private void execute_self(PlayerActor pa){
+        if(ClansPlugin.clanManager().isClanLeader(pa.getUniqueId()))
+            throw new CommandError("meta.command.leadership-needed");
+        ClanManager.Error e = ClansPlugin.clanManager().disbandClan(ClansPlugin.clanManager().getClan(pa));
+        switch(e){
+            case CLAN_NOT_FOUND -> throw new CommandError("meta.command.unknown-clan");
+            case CLAN_ALR_DISBAND -> throw new CommandError("command.disband.fail-deleted-self");
+            default -> LogContext.log(Level.WARN, "Unknown ClanManager error {0}. Contact developer", e);
+        }
+        // TODO: Fix this after adding components
+        ((CommandActor) pa).sendMessageT("command.disband.success-self");
     }
 
     @Override
