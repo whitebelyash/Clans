8af55613d8c263a509c2afb8615a9c34c0975ee6
whbex <66699673+whitebelyash@users.noreply.github.com>
2024-11-21 08:23:13 +0400

    Improve /clan disband command

--- clans-common/src/main/java/ru/whbex/develop/clans/common/cmd/clan/ClanDisbandCommand.java
+++ clans-common/src/main/java/ru/whbex/develop/clans/common/cmd/clan/ClanDisbandCommand.java
@@ -1,25 +1,40 @@
 package ru.whbex.develop.clans.common.cmd.clan;
 
+import org.slf4j.event.Level;
 import ru.whbex.develop.clans.common.ClansPlugin;
+import ru.whbex.develop.clans.common.clan.Clan;
 import ru.whbex.develop.clans.common.clan.ClanManager;
 import ru.whbex.develop.clans.common.cmd.CommandActor;
 import ru.whbex.develop.clans.common.cmd.exec.Command;
 import ru.whbex.develop.clans.common.cmd.exec.CommandError;
 import ru.whbex.develop.clans.common.cmd.exec.CommandUsageError;
+import ru.whbex.develop.clans.common.player.PlayerActor;
+import ru.whbex.lib.log.LogContext;
 
 public class ClanDisbandCommand implements Command {
     @Override
     public void execute(CommandActor actor, Command command, String label, String... args) {
         // WIP
+        boolean self = false;
         if(args.length < 2)
-            throw new CommandUsageError();
-        String tag = args[1];
-        ClanManager.Error e = ClansPlugin.Context.INSTANCE.plugin.getClanManager().disbandClan(tag);
+            self = true;
+        PlayerActor pa = (PlayerActor) actor;
+        if(self){
+            if(ClansPlugin.Context.INSTANCE.plugin.getClanManager().getClan(pa) == null)
+                throw new CommandError("meta.command.leadership-needed");
+        }
+        Clan clan = self ? ClansPlugin.Context.INSTANCE.plugin.getClanManager().getClan(pa) :
+                            ClansPlugin.Context.INSTANCE.plugin.getClanManager().getClan(args[0]);
+        if(clan == null)
+            throw new CommandError("meta.command.unknown-clan");
+        ClanManager.Error e = ClansPlugin.Context.INSTANCE.plugin.getClanManager().disbandClan(clan);
         if(e!=null)
             switch (e){
                 case CLAN_NOT_FOUND -> throw new CommandError("meta.command.unknown-clan");
+                case CLAN_ALR_DISBAND -> throw new CommandError(self ? "command.disband.fail-deleted" : "command.disband.fail-deleted-self");
+                default -> LogContext.log(Level.WARN, "Unknown ClanManager error {0}. Contact developer", e);
             }
-        actor.sendMessageT("command.disband.success");
+        actor.sendMessageT(self ? "command.disband.success-self" : "command.disband.success");
     }
 
     @Override
