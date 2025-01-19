27fc3409281919b6a02936f7a446763f4140b3b0
whbex <66699673+whitebelyash@users.noreply.github.com>
2025-01-20 02:13:57 +0400

    ListenerBukkit: add/remove Player object on login/leave

--- clans-bukkit/src/main/java/ru/whbex/develop/clans/bukkit/listener/ListenerBukkit.java
+++ clans-bukkit/src/main/java/ru/whbex/develop/clans/bukkit/listener/ListenerBukkit.java
@@ -6,15 +6,15 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import ru.whbex.develop.clans.bukkit.player.PlayerManagerBukkit;
+import ru.whbex.develop.clans.bukkit.player.PlayerActorBukkit;
 import ru.whbex.develop.clans.common.ClansPlugin;
 import ru.whbex.develop.clans.common.event.EventSystem;
-import ru.whbex.lib.log.Debug;
 
 public class ListenerBukkit implements Listener {
     @EventHandler(priority =  EventPriority.HIGHEST)
     public void on(PlayerLoginEvent e){
-        ClansPlugin.Context.INSTANCE.plugin.getPlayerManager().registerPlayerActor(e.getPlayer().getUniqueId());
+        PlayerActorBukkit actor = (PlayerActorBukkit) ClansPlugin.playerManager().registerPlayerActor(e.getPlayer().getUniqueId());
+        actor.setBukkitPlayer(e.getPlayer());
     }
     @EventHandler(priority = EventPriority.HIGHEST)
     public void on(PlayerJoinEvent e){
@@ -22,6 +22,9 @@ public class ListenerBukkit implements Listener {
     }
     @EventHandler(priority = EventPriority.HIGHEST)
     public void on(PlayerQuitEvent e){
+        // Remove playerobj
+        // TODO: Check for NPE
+        ((PlayerActorBukkit) ClansPlugin.playerManager().getPlayerActor(e.getPlayer().getUniqueId())).setBukkitPlayer(null);
         EventSystem.PLAYER_LEFT.call(e.getPlayer().getUniqueId());
     }
 }
