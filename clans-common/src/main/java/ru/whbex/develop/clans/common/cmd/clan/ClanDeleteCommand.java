3107e2eddfedc46276dc36d8e27402729de4729c
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2025-02-06 21:12:58 +0400

    Implement DeleteCommand

--- clans-common/src/main/java/ru/whbex/develop/clans/common/cmd/clan/ClanDeleteCommand.java
+++ clans-common/src/main/java/ru/whbex/develop/clans/common/cmd/clan/ClanDeleteCommand.java
@@ -1,11 +1,45 @@
 package ru.whbex.develop.clans.common.cmd.clan;
 
+import ru.whbex.develop.clans.common.ClansPlugin;
+import ru.whbex.develop.clans.common.clan.ClanManager;
 import ru.whbex.develop.clans.common.cmd.exec.Command;
 import ru.whbex.develop.clans.common.cmd.CommandActor;
+import ru.whbex.develop.clans.common.cmd.exec.CommandError;
+import ru.whbex.develop.clans.common.cmd.exec.CommandUsageError;
+import ru.whbex.lib.string.StringUtils;
+
+import java.util.UUID;
 
 public class ClanDeleteCommand implements Command {
     @Override
     public void execute(CommandActor actor, Command command, String label, String... args) {
+        if(args.length < 2)
+            throw new CommandUsageError();
+
+        if(!actor.hasData("cmd-delete-confirm")){
+            actor.sendMessageT("command.delete.warn");
+            actor.setData("cmd-delete-confirm", args[1]);
+            ClansPlugin.TaskScheduler().runLater(() -> actor.removeData("cmd-delete-confirm"), 2500L);
+        } else {
+            String f = (String) actor.getData("cmd-delete-confirm");
+            if(!f.equals(args[1])) {
+                actor.sendMessageT("command.delete.cancelled");
+                actor.removeData("cmd-delete-confirm");
+                return;
+            }
+            actor.removeData("cmd-delete-confirm");
+        }
+
+        String cid_s = args[1];
+        UUID cid;
+        if((cid = StringUtils.UUIDFromString(cid_s)) == null)
+            throw new CommandUsageError();
+        ClanManager.Error e = ClansPlugin.clanManager().removeClan(cid);
+        switch(e){
+            case SUCCESS -> actor.sendMessageT("command.delete.success");
+            case CLAN_SYNC_ERROR -> throw new CommandError(null);
+            case CLAN_NOT_FOUND -> actor.sendMessageT("meta.command-unknown-clan");
+        }
 
     }
 
