6e1828632c4e6a1d415bd4c3dd1cbe4bd9060dc3
whbex <66699673+whitebelyash@users.noreply.github.com>
2025-02-07 10:57:41 +0400

    ClanRank: add color

--- clans-common/src/main/java/ru/whbex/develop/clans/common/clan/ClanRank.java
+++ clans-common/src/main/java/ru/whbex/develop/clans/common/clan/ClanRank.java
@@ -2,16 +2,17 @@ package ru.whbex.develop.clans.common.clan;
 
 public enum ClanRank {
 
-    NOVICE("rank.novice"),
-    MEMBER("rank.member"),
-    RECRUITER("rank.recruiter"),
-    DEPUTY("rank.deputy"),
-    LEADER("rank.leader")
+    NOVICE("rank.novice", '7'),
+    MEMBER("rank.member", 'f'),
+    RECRUITER("rank.recruiter", 'b'),
+    DEPUTY("rank.deputy", '6'),
+    LEADER("rank.leader", 'c')
     ;
     public final String name;
+    public final char color;
 
-    ClanRank(String name){
+    ClanRank(String name, char color){
         this.name = name;
-
+        this.color = color;
     }
 }
