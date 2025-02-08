f47e4e6d44f77b6d5ca518590c07972b1d4b390a
whbex <66699673+whitebelyash@users.noreply.github.com>
2025-02-08 20:20:11 +0400

    Constants: formatting

--- clans-common/src/main/java/ru/whbex/develop/clans/common/Constants.java
+++ clans-common/src/main/java/ru/whbex/develop/clans/common/Constants.java
@@ -5,7 +5,6 @@ import ru.whbex.develop.clans.common.clan.ClanRank;
 import java.util.concurrent.TimeUnit;
 
 public class Constants {
-
     // Minecraft
     public static final char COLOR_SYMBOL = '§';
     public static final char PLAYER_COLOR_SYMBOL = '&';
@@ -15,7 +14,7 @@ public class Constants {
     public static final int NEXT_LEVEL_REQ = 128;
     public static final int NEXT_LEVEL_STEP = 64;
 
-    // ClanManager creation
+    // Clan name stub
     public static final String CLAN_NAME_FORMAT = "{0} clan";
 
     // Ranks
@@ -25,8 +24,4 @@ public class Constants {
     public static final String LANGUAGE_FILE_NAME = "messages.lang";
     public static final Long TASK_WAIT_TIMEOUT = 10L;
     public static final TimeUnit TASK_WAIT_TIMEOUT_UNIT = TimeUnit.SECONDS;
-
-
-
-
 }
