535fc0d9f94b9fab8bfae99a0c67c5b4a962df41
whbex <66699673+whitebelyash@users.noreply.github.com>
2025-02-08 20:19:35 +0400

    Config: cleanup

--- clans-bukkit/src/main/java/ru/whbex/develop/clans/bukkit/conf/ConfigBukkit.java
+++ clans-bukkit/src/main/java/ru/whbex/develop/clans/bukkit/conf/ConfigBukkit.java
@@ -1,6 +1,5 @@
 package ru.whbex.develop.clans.bukkit.conf;
 
-import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.slf4j.event.Level;
@@ -11,13 +10,11 @@ import ru.whbex.lib.log.LogContext;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Objects;
 
 public class ConfigBukkit implements Config {
     private final YamlConfiguration config;
     private final File configFile;
 
-    /* Settings */
     private DatabaseType dbType;
     private String dbAddress;
     private String dbName;
@@ -49,10 +46,6 @@ public class ConfigBukkit implements Config {
         dbPassword = config.getString("database.password", "12345");
         flushDelay = config.getLong("clan.flush-delay", 300);
         allowTransfer = config.getBoolean("clan.allow-transfer", true);
-    }
-    @Override
-    public boolean test() {
-        return false;
     }
 
     public void reload() throws IOException, InvalidConfigurationException {
