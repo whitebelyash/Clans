b70f7c7536a221c51aabae47bda70813485bff02
whbex <66699673+whitebelyash@users.noreply.github.com>
2025-02-10 10:03:09 +0400

    EventSystem: switch PlayerActor to CommandActor in ClanEvent

--- clans-common/src/main/java/ru/whbex/develop/clans/common/event/def/ClanEvent.java
+++ clans-common/src/main/java/ru/whbex/develop/clans/common/event/def/ClanEvent.java
@@ -1,15 +1,16 @@
 package ru.whbex.develop.clans.common.event.def;
 
 import ru.whbex.develop.clans.common.clan.Clan;
+import ru.whbex.develop.clans.common.cmd.CommandActor;
 import ru.whbex.develop.clans.common.event.AbstractEvent;
 import ru.whbex.develop.clans.common.event.EventHandler;
 import ru.whbex.develop.clans.common.player.PlayerActor;
 
 public class ClanEvent extends AbstractEvent<ClanEvent.ClanEventHandler> {
     public interface ClanEventHandler extends EventHandler {
-        void call(PlayerActor actor, Clan clan);
+        void call(CommandActor actor, Clan clan);
     }
-    public void call(PlayerActor actor, Clan clan){
+    public void call(CommandActor actor, Clan clan){
         handlerList.forEach(handler -> handler.call(actor, clan));
     }
 }
