136badf6495a8645776553bebc53a8054d24a2a3
whbex <66699673+whitebelyash@users.noreply.github.com>
2025-04-24 09:35:43 +0400

    Remove old PlayerManager

--- clans-bukkit/src/main/java/ru/whbex/develop/clans/bukkit/player/PlayerManagerBukkit.java
+++ clans-bukkit/src/main/java/ru/whbex/develop/clans/bukkit/player/PlayerManagerBukkit.java
@@ -3,173 +3,24 @@ package ru.whbex.develop.clans.bukkit.player;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import ru.whbex.develop.clans.common.cmd.CommandActor;
-import ru.whbex.develop.clans.common.event.EventSystem;
-import ru.whbex.develop.clans.common.event.def.PlayerEvent;
-import ru.whbex.develop.clans.common.misc.SQLUtils;
+import ru.whbex.develop.clans.common.player.ConsoleActor;
 import ru.whbex.develop.clans.common.player.PlayerActor;
 import ru.whbex.develop.clans.common.player.PlayerManager;
-import ru.whbex.develop.clans.common.player.ConsoleActor;
-import ru.whbex.develop.clans.common.player.PlayerProfile;
-import ru.whbex.develop.clans.common.task.DatabaseService;
-import ru.whbex.lib.log.Debug;
-import ru.whbex.lib.sql.SQLAdapter;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
-// TODO: Huge refactor
-public class PlayerManagerBukkit implements PlayerManager {
-    private final Map<UUID, PlayerActor> actors = new HashMap<>();
-    private final Map<String, PlayerActor> actorsN = new HashMap<>();
-    private final Map<UUID, PlayerActor> onlineActors = new HashMap<>();
-
-    private final ConsoleActorBukkit consoleActor = new ConsoleActorBukkit();
-
-
-
-    public PlayerManagerBukkit() {
-        this.createTables();
-        Debug.print("Registering event callbacks...");
-        EventSystem.PLAYER_JOIN.register(onJoin);
-        EventSystem.PLAYER_LEFT.register(onQuit);
-    }
-
-
+public class PlayerManagerBukkit extends PlayerManager {
+    private final ConsoleActor ca = new ConsoleActorBukkit();
     @Override
-    public PlayerActor getPlayerActor(UUID id) {
-        return actors.get(id);
+    public ConsoleActor consoleActor() {
+        return ca;
     }
 
     @Override
-    public PlayerActor getPlayerActor(String nickname) {
-        return actorsN.get(nickname);
+    public PlayerActor createActorObject(UUID uuid) {
+        return new PlayerActorBukkit(uuid);
     }
-
-    @Override
-    public PlayerActor getOnlinePlayerActor(UUID id) {
-        return onlineActors.get(id);
-    }
-
-    @Override
-    public PlayerActor registerPlayerActor(PlayerActor actor) {
-        if(actors.containsValue(actor))
-            return actor;
-        actors.put(actor.getUniqueId(), actor);
-        if(actor.getProfile() == null){
-            Debug.print("Player profile not set, fetching...");
-            // Set stub profile before real is fetched
-            actor.setProfile(new PlayerProfile(actor.getUniqueId(), null, -1, 0, null));
-            // Set name if online
-            if(actor.isOnline()) {
-                actor.getProfile().setName(((PlayerActorBukkit) actor).getBukkitPlayer().getName());
-                Debug.print("name set!");
-            }
-            // We'll ignore fetch progress for now
-            DatabaseService.getAsyncExecutor(SQLAdapter::preparedQuery)
-                    .sql(FETCH_PROFILE_STMT)
-                    .setPrepared(ps -> ps.setString(1, actor.getUniqueId().toString()))
-                    .queryCallback(resp -> {
-                        if(resp.resultSet().next())
-                            do {
-                                String old = actor.getProfile() != null ? actor.getProfile().getName() : null;
-                                actor.setProfile(SQLUtils.profileFromQuery(resp.resultSet()));
-                                if(old != null)
-                                    actor.getProfile().setName(old);
-                                actorsN.put(actor.getProfile().getName(), actor);
-                                Debug.print("Profile successfully fetched");
-                                break;
-                            } while(resp.resultSet().next());
-                        else {
-                            Debug.print("PlayerProfile not found for {0}", actor);
-                        }
-                        return null;
-                    })
-                    .executeAsync();
-        }
-        Debug.print("Registered/updated actor {0}", actor);
-        return actor;
-    }
-
-    @Override
-    public PlayerActor registerPlayerActor(UUID id) {
-        if(actors.containsKey(id)) {
-            // makeOnline(id);
-            return actors.get(id);
-        }
-        PlayerActor p = new PlayerActorBukkit(id);
-        return registerPlayerActor(p);
-    }
-    public void unregisterPlayerActor(UUID id) throws SQLException {
-        if(!actors.containsKey(id))
-            return;
-    }
-    private static void profileToPrepStatement(PlayerProfile pp, PreparedStatement ps) throws SQLException{
-        ps.setString(1, pp.getOwner().toString());
-        ps.setString(2, pp.getName());
-        ps.setLong(3, pp.getRegDate());
-        ps.setLong(4, pp.getLastSeen());
-    }
-    // TODO: Move to PlayerManager interface
-    public void savePlayerActor(UUID id){
-        if(!actors.containsKey(id))
-            return;
-        PlayerActor actor = actors.get(id);
-        Debug.print("Saving PlayerProfile for {0}", actor);
-        SQLAdapter<Void>.Executor<Void> ex = DatabaseService.getAsyncExecutor(SQLAdapter::preparedUpdate)
-                .setVerbose(true)
-                .setPrepared(ps -> {
-                    profileToPrepStatement(actor.getProfile(), ps);
-                })
-                .updateCallback(resp -> {
-                    Debug.print("Updated {0} rows", resp.updateResult());
-                    return null;
-                });
-        switch(DatabaseService.getDatabaseBackend()){
-            case H2 -> ex.sql(INSERT_PROFILE_STMT_H2);
-            case SQLITE -> ex.sql(INSERT_PROFILE_STMT_SQLITE);
-            default -> throw new IllegalArgumentException("Unsupported database backend " + DatabaseService.getDatabaseBackend());
-        }
-        ex.executeAsync();
-    }
-
-    @Override
-    public Collection<PlayerActor> getOnlinePlayerActors() {
-        return onlineActors.values();
-    }
-
-    private final PlayerEvent.PlayerEventHandler onJoin = (id) -> {
-        Debug.print("onJoin() " + id);
-        if (!actors.containsKey(id))
-            return;
-        PlayerActor actor = actors.get(id);
-        Debug.print("Updating player name...");
-        String bukkitName = ((PlayerActorBukkit) actor).getBukkitPlayer().getName();
-        if (actor.getProfile().getName() == null) {
-            actor.getProfile().setName(bukkitName);
-        }
-        if (actor.getProfile().getName().equals(bukkitName)) {
-            actorsN.remove(actor.getProfile().getName());
-            actor.getProfile().setName(bukkitName);
-        }
-        actorsN.put(bukkitName, actor);
-        onlineActors.put(id, actor);
-    };
-    private final PlayerEvent.PlayerEventHandler onQuit = ((id) -> {
-        Debug.print("onQuit() " + id);
-        onlineActors.remove(id);
-        savePlayerActor(id);
-    });
-
-    @Override
-    public ConsoleActor getConsoleActor() {
-        return consoleActor;
-    }
-
     public CommandActor asCommandActor(CommandSender sender){
-        return sender instanceof Player ? (CommandActor)this.registerPlayerActor(((Player) sender).getUniqueId()) : consoleActor;
+        return sender instanceof Player ? (CommandActor)this.loadPlayerActor(((Player) sender).getUniqueId()) : (CommandActor) ca;
     }
 }
--- clans-common/src/main/java/ru/whbex/develop/clans/common/player/PlayerManager.java
+++ clans-common/src/main/java/ru/whbex/develop/clans/common/player/PlayerManager.java
@@ -1,100 +1,221 @@
 package ru.whbex.develop.clans.common.player;
 
+import org.slf4j.event.Level;
 import ru.whbex.develop.clans.common.ClansPlugin;
+import ru.whbex.develop.clans.common.Constants;
+import ru.whbex.develop.clans.common.misc.SQLUtils;
 import ru.whbex.develop.clans.common.task.DatabaseService;
 import ru.whbex.lib.log.Debug;
+import ru.whbex.lib.log.LogContext;
 import ru.whbex.lib.sql.SQLAdapter;
 import ru.whbex.lib.string.StringUtils;
 
+import java.sql.ResultSet;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeoutException;
 
-public interface PlayerManager {
-    String TABLE_CREATE_STMT = "CREATE TABLE IF NOT EXISTS players(" +
-            "id varchar(18) UNIQUE NOT NULL, " +
-            "name varchar(16) NOT NULL, " +
-            "regDate bigint, " +
-            "lastSeen bigint" +
-            ");";
-    String FETCH_PROFILE_STMT = "SELECT * FROM players WHERE id=?;";
-    String INSERT_PROFILE_STMT_SQLITE = "INSERT OR REPLACE INTO players VALUES(?, ?, ?, ?);";
-    String INSERT_PROFILE_STMT_H2 = "MERGE INTO players VALUES(?, ?, ?, ?);";
+public abstract class PlayerManager {
+    private final Map<UUID, PlayerActor> actors = new HashMap<>();
+    private final Map<UUID, PlayerActor> onlineActors = new HashMap<>();
+    private final Map<String, UUID> nameIdMap = new HashMap<>();
+    private DatabaseBridge db;
 
+    public PlayerManager(){
+        Debug.tprint("PlayerManager", "initializing...");
+        db = new DatabaseBridge();
+        db.createTable();
+        Debug.tprint("PlayerManager", "Complete");
+    }
+    public PlayerActor playerActor(UUID uuid){
+        return actors.get(uuid);
+    }
+    public PlayerActor resolvePlayerActor(String name){
+        return nameIdMap.get(name) == null ? null : actors.get(nameIdMap.get(name));
+    }
+    public UUID resolveUUID(String name){
+        return nameIdMap.get(name);
+    }
 
+    public Collection<PlayerActor> getOnlineActors() {
+        return onlineActors.values();
+    }
 
-    /**
-     * Get player actor by uuid
-     * @param id actor's UUID
-     * @return actor if found, otherwise null
-     */
-    PlayerActor getPlayerActor(UUID id);
-
-    /**
-     * Get player actor by his nickname
-     * @param nickname name
-     * @return actor if found (loaded), otherwise null
-     */
-    PlayerActor getPlayerActor(String nickname);
-
-    /**
-     * Get online player actor
-     * @param id actor's UUID
-     * @return player actor or null if offline
-     */
-    PlayerActor getOnlinePlayerActor(UUID id);
-
-    /**
-     * Register player actor.
-     * @param actor actor
-     * @return registered actor
-     */
-    // TODO: Remove
-    PlayerActor registerPlayerActor(PlayerActor actor);
-
-    /**
-     * Register player actor by uuid.
-     * @param id actor's UUID
-     */
-    PlayerActor registerPlayerActor(UUID id);
-    /**
-     * Get all online actors.
-     * @return collection of actors.
-     */
-    Collection<PlayerActor> getOnlinePlayerActors();
-
-    /**
-     * Get console actor.
-     * @return console actor.
-     */
-    ConsoleActor getConsoleActor();
+    public Collection<PlayerActor> getActors() {
+        return actors.values();
+    }
 
     // Utils
-    default void broadcast(String string){
-        getOnlinePlayerActors().forEach(a -> a.sendMessage(string));
+    public void broadcast(String string){
+        onlineActors.values().forEach(a -> a.sendMessage(string));
     }
-    default void broadcast(String format, Object... args){
-        getOnlinePlayerActors().forEach(a -> a.sendMessage(StringUtils.simpleformat(format, args)));
+    public void broadcast(String format, Object... args){
+        onlineActors.values().forEach(a -> a.sendMessage(StringUtils.simpleformat(format, args)));
     }
-    default void broadcastT(String translatableString){
+    public void broadcastT(String translatableString){
         String t = ClansPlugin.mainLanguage().getPhrase(translatableString);
-        getOnlinePlayerActors().forEach(a -> {
+        onlineActors.values().forEach(a -> {
             a.sendMessage(t);
         });
     }
-    default void broadcastT(String translatableFormat, Object... args){
+    public void broadcastT(String translatableFormat, Object... args){
         String t = StringUtils.simpleformat(ClansPlugin.mainLanguage().getPhrase(translatableFormat), args);
-        getOnlinePlayerActors().forEach(a -> a.sendMessage(t));
+        onlineActors.values().forEach(a -> a.sendMessage(t));
     }
-    default void createTables(){
-        Debug.print("Creating players table...");
-        DatabaseService.getExecutor(SQLAdapter::update)
-                .sql(TABLE_CREATE_STMT)
-                .setVerbose(true)
-                .updateCallback(resp -> {
-                    Debug.print("Updated {0} rows", resp.updateResult());
-                    return null;
-                })
-                .exceptionally(e -> {throw new RuntimeException(e);})
-                .execute();
+
+    public PlayerActor loadPlayerActor(UUID uuid){
+        if(actors.containsKey(uuid))
+            return actors.get(uuid);
+        // Actor is non-existent
+        // Bukkit#getPlayer returns null here if everything is being handled on login
+        PlayerActor a = createActorObject(uuid);
+        return loadPlayerActor(a, false);
     }
+
+    // with login playerobj must be available on actor
+    public PlayerActor loadPlayerActor(PlayerActor a, boolean login){
+        UUID uuid = a.getUniqueId();
+        actors.put(uuid, a);
+        long currentTime = System.currentTimeMillis()/1000L;
+        // Set a stub profile before real is fetched
+        PlayerProfile stubProfile = new PlayerProfile(uuid, null, currentTime, currentTime, null);
+        a.setProfile(stubProfile);
+        boolean online = a.isOnline();
+        if(online || login) {
+            onlineActors.put(uuid, a);
+            stubProfile.setName(a.getOnlineName());
+            nameIdMap.put(stubProfile.getName(), a.getUniqueId());
+        }
+        Future<PlayerProfile> playerProfileFetcher = db.loadProfileAsync(uuid);
+        a.bindFetcher(playerProfileFetcher);
+        // TODO: Move somewhere?
+        ClansPlugin.taskScheduler().runAsync(() -> {
+            try {
+                Debug.lprint("Waiting on Future<PlayerProfile> for {0}...", uuid);
+                PlayerProfile pp = playerProfileFetcher.get(Constants.TASK_WAIT_TIMEOUT, Constants.TASK_WAIT_TIMEOUT_UNIT);
+                if(pp != null) {
+                    a.setProfile(pp);
+                }
+                Debug.lprint("Profile data fetched for {0}!", uuid);
+                // Save player profile if player wasn't registered before
+                if(pp == null && (online || login)){
+                    // TODO: Autoinsert values in loadProfile()
+                    db.insertProfile(uuid);
+                }
+                // TODO: Update player nickname in db
+                else Debug.print("PlayerManager", "Skipping actor insert");
+
+            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                LogContext.log(Level.ERROR, "Unable to load actor data for UUID " + uuid + "! See below stacktrace for more info");
+                e.printStackTrace();
+            }
+        });
+        return a;
+    }
+    public void unloadPlayerActor(UUID uuid){
+        if(!actors.containsKey(uuid))
+            throw new NullPointerException("No such actor " + uuid + "!");
+        if(actors.get(uuid).getProfile().getClanId() != null)
+            throw new IllegalArgumentException("Actor " + uuid + " has a clan bound to it!");
+
+        // Just remove actor from maps for now
+        onlineActors.remove(uuid);
+        String n = actors.remove(uuid).getName();
+        nameIdMap.remove(n);
+    }
+
+    public abstract PlayerActor createActorObject(UUID uuid);
+    public abstract ConsoleActor consoleActor();
+
+
+    // Same logic as in ClanManager
+    private class DatabaseBridge {
+        private DatabaseBridge(){
+
+        }
+        private void createTable(){
+            DatabaseService.getExecutor(SQLAdapter::update)
+                    .sql("CREATE TABLE IF NOT EXISTS actors(" +
+                            "id varchar(36) PRIMARY KEY NOT NULL UNIQUE, " +
+                            "name varchar(32),   " +
+                            "regDate BIGINT, " +
+                            "lastSeen BIGINT, " +
+                            "cid varchar(18)" +
+                            ");")
+                    .exceptionally(e -> {
+                        LogContext.log(Level.ERROR, "Failed to initialize players database table! See below stacktrace for more info");
+                        e.printStackTrace();
+                    })
+                    .execute();
+        }
+        private PlayerProfile loadProfile(UUID uuid){
+            return DatabaseService.getExecutor(PlayerProfile.class, SQLAdapter::preparedQuery)
+                    .sql("SELECT * FROM actors WHERE id=?;")
+                    .setPrepared(ps -> ps.setString(1, uuid.toString()))
+                    .queryCallback(resp -> {
+                        ResultSet r = resp.resultSet();
+                        PlayerProfile ret;
+                        if(r.next())
+                            do {
+                                Debug.lprint("Found PlayerProfile!");
+                                ret = SQLUtils.profileFromQuery(r);
+                            } while (r.next());
+                        else {
+                            Debug.lprint("Profile data was not found for {0} :(", uuid);
+                            return null;
+                        }
+                        return ret;
+                    })
+                    .execute();
+        }
+        private Future<PlayerProfile> loadProfileAsync(UUID uuid){
+            return DatabaseService.getAsyncExecutor(PlayerProfile.class, SQLAdapter::preparedQuery)
+                    .sql("SELECT * FROM actors WHERE id=?;")
+                    .setPrepared(ps -> ps.setString(1, uuid.toString()))
+                    .queryCallback(resp -> {
+                        ResultSet r = resp.resultSet();
+                        PlayerProfile ret;
+                        int i = 0;
+                        if(r.next())
+                            do {
+                                i++;
+                                Debug.lprint("Profile found, iter: {0}", i);
+                                ret =  SQLUtils.profileFromQuery(r);
+                            } while (r.next());
+                        else {
+                            Debug.lprint("Profile data was not found for {0} :(", uuid);
+                            return null;
+                        }
+                        return ret;
+                    })
+                    .executeAsync();
+        }
+
+        private void insertProfile(UUID uuid){
+            if(actors.get(uuid) == null)
+                throw new NullPointerException("No such actor " + uuid + "!");
+            if(actors.get(uuid).getProfile() == null)
+                throw new NullPointerException("PlayerProfile on " + uuid + " is null!");
+            DatabaseService.getExecutor(SQLAdapter::preparedUpdate)
+                    .sql("INSERT INTO actors VALUES(?,?,?,?,?);")
+                    .setPrepared(ps -> SQLUtils.profileToPrepStatement(ps, actors.get(uuid).getProfile()))
+                    .execute();
+        }
+        private Future<Void> insertProfileAsync(UUID uuid){
+            if(actors.get(uuid) == null)
+                throw new NullPointerException("No such actor " + uuid + "!");
+            if(actors.get(uuid).getProfile() == null)
+                throw new NullPointerException("PlayerProfile on " + uuid + " is null!");
+            return DatabaseService.getAsyncExecutor(SQLAdapter::preparedUpdate)
+                    .sql("INSERT INTO actors VALUES(?,?,?,?,?);")
+                    .setPrepared(ps -> SQLUtils.profileToPrepStatement(ps, actors.get(uuid).getProfile()))
+                    .executeAsync();
+        }
+    }
+
+
 }
